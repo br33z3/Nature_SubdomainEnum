334f96dc9dc3d94947e99c691f27f95b73346215
Berk Can <31792851+BreeZeQQ@users.noreply.github.com>
2020-07-08 17:04:16 -0400

    Added install.sh for easy install

---
+++ install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+
+echo "[!]Installing Sublist3r"
+apt-get install sublist3r > /dev/null
+echo "[+]Sublist3r Installed Successfully"
+echo "[!]Downloading Aquatone_1"
+wget "https://github.com/michenriksen/aquatone/releases/download/v1.7.0/aquatone_linux_amd64_1.7.0.zip" > /dev/null
+echo "[+]Downloaded Aquatone_1"
+echo "[!]Unzipping..."
+unzip aquatone_linux_amd64_1.7.0.zip
+echo "[+]Unzipped"
+mv aquatone /usr/bin/aquatone
+echo "[+]Aquatone_1 Installed Successfully"
+echo "[!]Installing Amass"
+apt-get install amass > /dev/null
+echo "[+]Amass Installed Successfully"
+echo "[!]Installing Aquatone_2"
+gem install aquatone > /dev/null
+echo "[+]Aquatone_2 Installed"
+echo "[!]Installing golang for Subfinder"
+apt-get install golang > /dev/null
+echo "[+]golang Installed Successfully"
+echo "[!]Installing Subfinder"
+GO111MODULE=on go get -v github.com/projectdiscovery/subfinder/cmd/subfinder > /dev/null
+echo "[+]Subfinder Installed Successfully"
+echo "---------------------------------------"
+echo "ALL requirements installed Successfully"
+echo "---------------------------------------"
