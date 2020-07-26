52cd44c4927209ee5ab2133886df3ea69ee856d4
Berk Can <31792851+BreeZeQQ@users.noreply.github.com>
2020-07-26 23:11:18 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -24,6 +24,9 @@ echo "[+]golang Installed Successfully"
 echo "[!]Installing Subfinder"
 GO111MODULE=on go get -v github.com/projectdiscovery/subfinder/cmd/subfinder > /dev/null
 echo "[+]Subfinder Installed Successfully"
+echo "[!]Installing DNSMap"
+apt-get install dnsmap > /dev/null
+echo "[+]DNSMap Installed Successfully"
 rm LICENSE.txt
 rm aquatone_linux_amd64_1.7.0.zip
 rm README.md
