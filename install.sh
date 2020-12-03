f9b69b372d08d3e2b4d77cfe09717e00d683cff2
Berk Can <31792851+BreeZeQQ@users.noreply.github.com>
2020-12-03 21:27:53 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 
 echo "[!]Installing Sublist3r"
-apt-get install sublist3r > /dev/null
+apt-get install sublist3r -y > /dev/null
 echo "[+]Sublist3r Installed Successfully"
 echo "[!]Downloading Aquatone_1"
 wget "https://github.com/michenriksen/aquatone/releases/download/v1.7.0/aquatone_linux_amd64_1.7.0.zip" > /dev/null
@@ -13,23 +13,23 @@ echo "[+]Unzipped"
 mv aquatone /usr/bin/aquatone
 echo "[+]Aquatone_1 Installed Successfully"
 echo "[!]Installing Amass"
-apt-get install amass > /dev/null
+apt-get install amass -y > /dev/null
 echo "[+]Amass Installed Successfully"
 echo "[!]Installing Aquatone_2"
 gem install aquatone > /dev/null
 echo "[+]Aquatone_2 Installed"
 echo "[!]Installing golang for Subfinder"
-apt-get install golang > /dev/null
+apt-get install golang -y > /dev/null
 echo "[+]golang Installed Successfully"
 echo "[!]Installing Subfinder"
 GO111MODULE=on go get -v github.com/projectdiscovery/subfinder/cmd/subfinder > /dev/null
 cp ~/go/bin/subfinder /bin/subfinder
 echo "[+]Subfinder Installed Successfully"
 echo "[!]Installing DNSMap"
-apt-get install dnsmap > /dev/null
+apt-get install dnsmap -y > /dev/null
 echo "[+]DNSMap Installed Successfully"
 echo "[!]Installing Fierce"
-apt-get install fierce > /dev/null
+apt-get install fierce -y > /dev/null
 echo "[+]Fierce Installed Successfully"
 rm LICENSE.txt
 rm aquatone_linux_amd64_1.7.0.zip
